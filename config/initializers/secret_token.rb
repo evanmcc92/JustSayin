5f2bb7cf8db911c9dff49e2db5b5eb744c882361
Evan McCullough <webmaster@evanamccullough.com>
2013-10-07 11:44:20 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SocialLiving::Application.config.secret_key_base = 'c1e116951caed609757e528f077172650a9c8685e1a3fc5a16a614d081a374fc8f8fe273e554367338970f04268a43ff731fc1b9833d5e031499f6fa7b88809d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SocialLiving::Application.config.session_store :cookie_store, key: '_SocialLiving_session'
