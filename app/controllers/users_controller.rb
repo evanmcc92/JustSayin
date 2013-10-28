5c2c3d32a29c2c43c9bcc746a3da2807b3992b19
Evan McCullough <webmaster@evanamccullough.com>
2013-10-28 11:50:05 -0400

    updates

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
       if @user.save
         session[:user_id] = @user.id
         UserMailer.welcome_email(@user).deliver
-        redirect_to @user
+        redirect_to root_url
       else
         render :action => "new"
       end
